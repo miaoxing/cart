dba079ad46039c0da68c4dda5a9354fe9c947f1c
twinh <twinhuang@qq.com>
2019-02-07 23:18:58 +0800

    refactor: nav更新为bs4

--- resources/views/carts/index-tab.php
+++ resources/views/carts/index-tab.php
@@ -1,8 +1,8 @@
 <!-- 超过一种购物车类型的需要显示tab -->
 <?php if (count($types) > 1) : ?>
   <ul class="header-tab nav tab-underline border-bottom">
-    <li class="border-primary <?= $req['type'] == 'default' ? 'active' : '' ?>">
-      <a class="text-active-primary" href="<?= $url('carts', ['type' => 'default']) ?>">
+    <li class="nav-item border-primary <?= $req['type'] == 'default' ? 'active' : '' ?>">
+      <a class="nav-link text-active-primary" href="<?= $url('carts', ['type' => 'default']) ?>">
         普通商品(<?= $types['default']->length() ?>)</a>
     </li>
     <?php $event->trigger('showCartTab', [$types, $req]) ?>
