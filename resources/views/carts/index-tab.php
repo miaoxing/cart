3315d406cba17aa75218524112c5d54f8a53cb7e
twinh <twinhuang@qq.com>
2019-03-06 16:35:00 +0800

    refactoring: tab => nav

--- resources/views/carts/index-tab.php
+++ resources/views/carts/index-tab.php
@@ -1,6 +1,6 @@
 <!-- 超过一种购物车类型的需要显示tab -->
 <?php if (count($types) > 1) : ?>
-  <ul class="header-tab nav tab-underline">
+  <ul class="header-nav nav nav-underline">
     <li class="nav-item">
       <a class="nav-link <?= $req['type'] == 'default' ? 'active' : '' ?> text-active-primary border-active-primary"
         href="<?= $url('carts', ['type' => 'default']) ?>">
