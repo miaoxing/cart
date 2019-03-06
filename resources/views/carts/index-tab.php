59953fa665d1c9114b80502fe712dc6493b7d40e
twinh <twinhuang@qq.com>
2019-03-06 15:32:34 +0800

    refactoring: 头部导航更新

--- resources/views/carts/index-tab.php
+++ resources/views/carts/index-tab.php
@@ -1,6 +1,6 @@
 <!-- 超过一种购物车类型的需要显示tab -->
 <?php if (count($types) > 1) : ?>
-  <ul class="header-tab nav tab-underline border-bottom">
+  <ul class="header-tab nav tab-underline">
     <li class="nav-item border-primary <?= $req['type'] == 'default' ? 'active' : '' ?>">
       <a class="nav-link text-active-primary" href="<?= $url('carts', ['type' => 'default']) ?>">
         普通商品(<?= $types['default']->length() ?>)</a>
