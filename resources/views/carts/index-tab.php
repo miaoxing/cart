b39a4cb07fa8666e9fe5c8eeb4626817082e8171
twinh <twinhuang@qq.com>
2019-03-06 16:26:02 +0800

    refactoring: tab active 更新

--- resources/views/carts/index-tab.php
+++ resources/views/carts/index-tab.php
@@ -1,8 +1,9 @@
 <!-- 超过一种购物车类型的需要显示tab -->
 <?php if (count($types) > 1) : ?>
   <ul class="header-tab nav tab-underline">
-    <li class="nav-item border-primary <?= $req['type'] == 'default' ? 'active' : '' ?>">
-      <a class="nav-link text-active-primary" href="<?= $url('carts', ['type' => 'default']) ?>">
+    <li class="nav-item">
+      <a class="nav-link <?= $req['type'] == 'default' ? 'active' : '' ?> text-active-primary border-active-primary"
+        href="<?= $url('carts', ['type' => 'default']) ?>">
         普通商品(<?= $types['default']->length() ?>)</a>
     </li>
     <?php $event->trigger('showCartTab', [$types, $req]) ?>
