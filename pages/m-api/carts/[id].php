6f6dbfd0708e0d559637216502e90fe14c566efa
twinh <twinhuang@qq.com>
2021-06-20 21:42:44 +0800

    feat(cart): 增加删除购物车接口

--- pages/m-api/carts/[id].php
+++ pages/m-api/carts/[id].php
@@ -1,11 +1,19 @@
 <?php
 
 use Miaoxing\Cart\Service\Cart;
+use Miaoxing\Cart\Service\CartModel;
 use Miaoxing\Plugin\BaseController;
 
 return new class extends BaseController {
     public function patch($req)
     {
         return Cart::update($req);
+    }
+
+    public function delete($req)
+    {
+        CartModel::mine()->findOrFail($req['id'])->destroy();
+
+        return suc();
     }
 };
