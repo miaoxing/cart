d61837648f9dece31ace4fbeea449b973063cd81
twinh <twinhuang@qq.com>
2021-06-26 19:40:44 +0800

    refactor(Cast): 更新生成的注释，自增 `id` 允许为 null

--- src/Metadata/CartTrait.php
+++ src/Metadata/CartTrait.php
@@ -11,8 +11,8 @@ namespace Miaoxing\Cart\Metadata;
  * @property int $orderId 订单编号
  * @property int $status 状态。1:正常;2:已下单;3:已删除
  * @property int $quantity 数量
- * @property float|null $changedPrice 修改后价格
- * @property float $addedPrice 加入价格
+ * @property string|null $changedPrice 修改后价格
+ * @property string $addedPrice 加入价格
  * @property string $configs 配置
  * @property string|null $createdAt
  * @property string|null $updatedAt
