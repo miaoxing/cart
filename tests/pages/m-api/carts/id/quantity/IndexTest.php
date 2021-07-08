9b21870a348556a848a5aec7b088a7bf2dbcd6ad
twinh <twinhuang@qq.com>
2021-07-08 18:32:02 +0800

    test(cart): 增加更改购物车数量接口

---
+++ tests/pages/m-api/carts/id/quantity/IndexTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace MiaoxingTest\Cart\Pages\MApi\Carts\Id\Quantity;
+
+use Miaoxing\Cart\Service\Cart;
+use Miaoxing\Plugin\Service\Tester;
+use Miaoxing\Plugin\Test\BaseTestCase;
+use Miaoxing\Product\Service\ProductModel;
+
+class IndexTest extends BaseTestCase
+{
+    public function testPut()
+    {
+        // 创建测试商品
+        $product = $this->createProduct([
+            'name' => '更改购物车的商品',
+        ], [
+            'price' => 20,
+            'stockNum' => 10,
+        ]);
+
+        ['data' => $cart] = $ret = Cart::create([
+            'skuId' => $product->skus[0]->id,
+            'quantity' => 1,
+        ]);
+        $this->assertRetSuc($ret);
+
+        $ret = Tester::request(['quantity' => 5])->put(sprintf('/m-api/carts/%s/quantity', $cart->id));
+        $this->assertRetSuc($ret);
+
+        $cart->reload();
+        $this->assertSame(5, $cart->quantity);
+
+        $ret = Tester::request(['quantity' => 3])->put(sprintf('/m-api/carts/%s/quantity', $cart->id));
+        $this->assertRetSuc($ret);
+
+        $cart->reload();
+        $this->assertSame(3, $cart->quantity);
+    }
+
+    protected function createProduct(array $data = [], array $sku = []): ProductModel
+    {
+        // 创建测试商品
+        $ret = Tester::postAdminApi('products', array_merge([
+            'name' => '测试商品',
+            'spec' => [
+                'specs' => ProductModel::getDefaultSpecs(),
+            ],
+            'skus' => [
+                array_merge([
+                    'price' => 20,
+                    'stockNum' => 10,
+                    'specValues' => [
+                        [
+                            'name' => '默认',
+                            'specName' => '默认',
+                        ],
+                    ],
+                ], $sku),
+            ],
+        ], $data));
+
+        /** @var ProductModel $product */
+        $product = $ret->getMetadata('model');
+
+        return $product;
+    }
+}
