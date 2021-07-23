94d6980dfbfb23509b67331686416a4194865929
twinh <twinhuang@qq.com>
2021-07-23 23:18:16 +0800

    test(cart): 测试选择购物车功能

--- pages/m/carts/index.test.js
+++ pages/m/carts/index.test.js
@@ -1,10 +1,11 @@
 import Index from './index';
-import {render, waitFor} from '@testing-library/react';
+import {render, waitFor, fireEvent} from '@testing-library/react';
 import $, {Ret} from 'miaoxing';
 import {createPromise, bootstrap, setUrl, resetUrl} from '@mxjs/test';
 import * as TaroTest from '@tarojs/taro';
 import {createProduct} from '@miaoxing/product/test-utils';
 import pretty from 'pretty';
+import Taro from '@tarojs/taro';
 
 bootstrap();
 let didShow;
@@ -106,6 +107,134 @@ describe('Index', () => {
     const html = pretty(container.innerHTML.replace(/id="swipeAction(.+?)"/g, 'id="REPLACED"'));
 
     expect(html).toMatchSnapshot();
+    expect($.http).toMatchSnapshot();
+  });
+
+  test('select', async () => {
+    const product = createProduct();
+
+    const promise = createPromise();
+
+    $.http = jest.fn()
+      .mockImplementationOnce(() => promise.resolve({
+        ret: Ret.suc({
+          data: [
+            {
+              id: 1,
+              productId: product.id,
+              skuId: product.skus[0].id,
+              quantity: 9,
+              changedPrice: null,
+              addedPrice: '9',
+              configs: {},
+              updatedAt: '2021-06-28 16:45:59',
+              sku: product.skus[0],
+              product: product,
+              createOrder: Ret.suc(),
+            },
+            {
+              id: 2,
+              productId: product.id,
+              skuId: product.skus[1].id,
+              quantity: 9,
+              changedPrice: null,
+              addedPrice: '9',
+              configs: {},
+              updatedAt: '2021-06-28 16:45:59',
+              sku: product.skus[1],
+              product: product,
+              createOrder: Ret.err('该商品已失效'),
+            },
+            {
+              id: 3,
+              productId: product.id,
+              skuId: product.skus[2].id,
+              quantity: 9,
+              changedPrice: null,
+              addedPrice: '9',
+              configs: {},
+              updatedAt: '2021-06-28 16:45:59',
+              sku: product.skus[2],
+              product: product,
+              createOrder: Ret.suc(),
+            },
+          ],
+          selected: [1, 3],
+        }),
+      }));
+
+    const {container, getByText, findByText} = render(<Index/>);
+    didShow();
+
+    const checkout = await findByText('结算（2）');
+    const amount = getByText('合计：');
+
+    const checkboxes = container.querySelectorAll('taro-checkbox-core');
+    const checkboxGroups = container.querySelectorAll('taro-checkbox-group-core');
+    expect(checkboxes.length).toBe(4);
+
+    // 默认选中1，3
+    expect(checkboxes[0].checked).toBeTruthy();
+    expect(checkboxes[0].disabled).toBeUndefined();
+
+    expect(checkboxes[1].checked).toBeUndefined();
+    expect(checkboxes[1].disabled).toBeTruthy();
+
+    expect(checkboxes[2].checked).toBeTruthy();
+    expect(checkboxes[2].disabled).toBeUndefined();
+
+    expect(checkout.textContent).toBe('结算（2）');
+    expect(amount.textContent).toBe('合计：￥' + (9 * 9 + 12 * 9));
+
+    // 取消选择1
+    fireEvent(checkboxGroups[0], new CustomEvent('change', {
+      detail: {
+        value: [3],
+      },
+    }));
+    expect(checkboxes[0].checked).toBeUndefined();
+    expect(checkboxes[1].checked).toBeUndefined();
+    expect(checkboxes[2].checked).toBeTruthy();
+
+    expect(checkout.textContent).toBe('结算（1）');
+    expect(amount.textContent).toBe('合计：￥' + (12 * 9));
+
+    // 全选
+    fireEvent.change(checkboxGroups[1]);
+    expect(checkboxes[0].checked).toBeTruthy();
+    expect(checkboxes[1].checked).toBeUndefined();
+    expect(checkboxes[2].checked).toBeTruthy();
+
+    expect(checkout.textContent).toBe('结算（2）');
+    expect(amount.textContent).toBe('合计：￥' + (9 * 9 + 12 * 9));
+
+    // 取消全选
+    fireEvent.change(checkboxGroups[1]);
+    expect(checkboxes[0].checked).toBeUndefined();
+    expect(checkboxes[1].checked).toBeUndefined();
+    expect(checkboxes[2].checked).toBeUndefined();
+
+    expect(checkout.textContent).toBe('结算（0）');
+    expect(amount.textContent).toBe('合计：￥0');
+
+    // 选择1
+    fireEvent(checkboxGroups[0], new CustomEvent('change', {
+      detail: {
+        value: [1],
+      },
+    }));
+    expect(checkboxes[0].checked).toBeTruthy();
+    expect(checkboxes[1].checked).toBeUndefined();
+    expect(checkboxes[2].checked).toBeUndefined();
+
+    expect(checkout.textContent).toBe('结算（1）');
+    expect(amount.textContent).toBe('合计：￥' + (9 * 9));
+
+    // 提交
+    Taro.navigateTo = jest.fn();
+    fireEvent.click(checkout);
+    await waitFor(() => expect(Taro.navigateTo).toBeCalled());
+
     expect($.http).toMatchSnapshot();
   });
 });
